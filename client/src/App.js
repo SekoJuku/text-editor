46d622b8747aaabe840e4e8e0632228a550ed624
SekoJuku <70684876+SekoJuku@users.noreply.github.com>
2021-03-22 17:12:49 +0600

    React+MaterializeCSS

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'materialize-css'
+
+function App() {
+  return (
+      <div className="container">
+        <h1>Hello</h1>
+      </div>
+  )
+}
+
+export default App;
